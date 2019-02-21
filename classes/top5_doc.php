dd6c179ca5df1fe61a86db915fd1a895fba18691
Maxim Stomphorst <maj@Maj-MBP.local>
2019-02-21 09:28:36 +0100

    top5 page working

---
+++ classes/top5_doc.php
@@ -0,0 +1,48 @@
+<?php
+include_once "classes/basic_doc.php";
+
+class Top5Doc extends basicDoc
+{
+    public function __construct($mydata)
+    {
+        // pass the data on to our parent class (basicDoc)
+        parent::__construct($mydata);
+    }
+
+
+    public function mainContent()
+    {
+        echo '
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Amount sold</th>
+                </tr>
+            </thead>
+        <tbody>';
+        $rank = 1;
+        foreach ($this->data['top5'] as $product) {
+            $this->showRow($product, $rank);
+            $rank++;
+        }
+
+        echo '</tbody>
+        </table>';
+    }
+
+    public function showRow($product, $rank)
+    {
+        echo '
+    <tr>
+      <th scope="row">' . $rank . '</th>
+      <td>
+        <a href="?page=detailProduct&id=' . $product['product_id'] . '" title="button">' . $product['name'] . '</a>
+      </td>
+      <td>' . $product['sum_amount'] . '</td>
+    </tr>';
+
+    }
+
+}
--- index.php
+++ index.php
@@ -34,7 +34,7 @@ function procesRequest($page)
                 try {
                     $data = validateRegisterForm();
 
-                    if (registerUser($data['name'],$data['email'],$data['password'])) {
+                    if (registerUser($data['name'], $data['email'], $data['password'])) {
                         $page = 'login';
                     } else {
                         $data["userExists"] = 'user is allready in database';
@@ -42,8 +42,8 @@ function procesRequest($page)
                 } catch (\Throwable $th) {
                     $data['errorMessage'] = $th->getMessage();
                     $page = 'home';
-                } 
-            } 
+                }
+            }
             break;
         case 'login':
             require_once 'incl/login.php';
@@ -55,12 +55,12 @@ function procesRequest($page)
                     // check if the login form is complete
                     if (getArrayVar($data, 'valid', false)) {
 
-                        if (validateUser($data['email'],$data['password'])) {
+                        if (validateUser($data['email'], $data['password'])) {
 
                             $userInfo = findUserByEmail($data['email']);
                             login($userInfo);
                             $page = 'home';
-                            
+
                         } else {
                             $data['LoginErr'] = "Email and password combination invalid";
                         }
@@ -85,7 +85,7 @@ function procesRequest($page)
                 $data['errorMessage'] = $th->getMessage();
                 $page = 'home';
             }
-            $page = 'products';   
+            $page = 'products';
             break;
         case 'detailProduct':
             try {
@@ -102,7 +102,7 @@ function procesRequest($page)
                 $data['errorMessage'] = $th->getMessage();
                 $page = 'home';
             }
-            
+
             break;
         case 'cart':
             require_once 'incl/cart.php';
@@ -117,10 +117,10 @@ function procesRequest($page)
             $data['top5'] = getTop5Sold();
             $page = 'top5';
             break;
-            
+
         case 'ajax':
             require_once 'incl/ajax.php';
-            
+
             if ($isPost) {
                 handleAjaxActions();
             }
@@ -128,15 +128,14 @@ function procesRequest($page)
             break;
     }
 
-
     $data['page'] = $page;
-    $data['menuLeft'] = array('home' => 'Home','about' => 'About', 'contact' => 'Contact', 'products' => 'Products', 'top5' => 'Top 5');
-    
+    $data['menuLeft'] = array('home' => 'Home', 'about' => 'About', 'contact' => 'Contact', 'products' => 'Products', 'top5' => 'Top 5');
+
     if (isLoggedIn()) {
         $data['loggedIn'] = true;
         $data['menuLeft']['cart'] = 'ShoppingCart';
         $username = getLoggedInUserName();
-        $data['menuRight'] = array('logout' => 'Logout '. ucfirst($username));
+        $data['menuRight'] = array('logout' => 'Logout ' . ucfirst($username));
     } else {
         $data['loggedIn'] = false;
         $data['menuRight'] = array('login' => 'Login', 'register' => 'Register');
@@ -145,6 +144,7 @@ function procesRequest($page)
     return $data;
 
 }
+
 
 // returns the name(string) of the requested page
 function getRequestedPage()
@@ -182,6 +182,11 @@ function showResponsePage($data)
             $view = new ProductsDoc($data);
             $view->show();
             break;
+        case 'top5':
+            require_once 'classes/top5_doc.php';
+            $view = new Top5Doc($data);
+            $view->show();
+            break;
         case 'login':
             require_once 'classes/login_doc.php';
             $view = new LoginDoc($data);
@@ -192,7 +197,7 @@ function showResponsePage($data)
             $view = new RegisterDoc($data);
             $view->show();
             break;
-        
+
         default:
             var_dump('NOO!');
             beginDocument();
@@ -201,9 +206,9 @@ function showResponsePage($data)
             break;
     }
 
-
-
 }
+
+
 
 // echo's the body of the html
 function showBodySection($data)
