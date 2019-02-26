030f2d8897231dd2fc0eda0c311ca8b0169e79c8
Maxim Stomphorst <maj@Maj-MBP.local>
2019-02-26 13:50:26 +0100

    crud working

--- incl/crud.php
+++ incl/crud.php
@@ -4,139 +4,117 @@ define('dbName', "educom");
 define('dbUser', "php_user");
 define('dbPassword', "password");
 
-interface iCRUD
-{
-    #============================================================================
-    # Insert a row of values into from the database
-    #
-    # @param string $sql the SQL string with for example ':email' for parameters
-    # @param array  $bindParameters associative array ( 'email' => 'joe@a.b' );
-    #
-    # @return int the inserted id or 0 if failed
-    #============================================================================
-   function createRow($sql, $bindParameters);
+// interface iCRUD
+// {
+//     #============================================================================
+//     # Insert a row of values into from the database
+//     #
+//     # @param string $sql the SQL string with for example ':email' for parameters
+//     # @param array  $bindParameters associative array ( 'email' => 'joe@a.b' );
+//     #
+//     # @return int the inserted id or 0 if failed
+//     #============================================================================
+//     public function createRow($sql, $bindParameters);
 
-    #============================================================================
-    # Read an array of objects from the database
-    #
-    # @param string $sql the SQL string with for example ':email' for parameters
-    # @param array  $bindParameters associative array ( 'email' => 'joe@a.b' );
-    #
-    # @return array (associative) array of objects or an empty array
-    #============================================================================
-    function readMultiRows($sql, $bindParameters);
+//     #============================================================================
+//     # Read an array of objects from the database
+//     #
+//     # @param string $sql the SQL string with for example ':email' for parameters
+//     # @param array  $bindParameters associative array ( 'email' => 'joe@a.b' );
+//     #
+//     # @return array (associative) array of objects or an empty array
+//     #============================================================================
+//     function readMultiRows($sql, $bindParameters);
 
-    #============================================================================
-    # Read one object from the database
-    #
-    # @param string $sql the SQL string with for example ':email' for parameters
-    # @param array  $bindParameters associative array ( 'email' => 'joe@a.b' );
-    #
-    # @return object the object found or NULL otherwise
-    #============================================================================
-    function readOneRow($sql, $bindParameters);
+//     #============================================================================
+//     # Read one object from the database
+//     #
+//     # @param string $sql the SQL string with for example ':email' for parameters
+//     # @param array  $bindParameters associative array ( 'email' => 'joe@a.b' );
+//     #
+//     # @return object the object found or NULL otherwise
+//     #============================================================================
+//     function readOneRow($sql, $bindParameters);
 
-    #============================================================================
-    # Update values into from the database
-    #
-    # @param string $sql the SQL string with for example ':email' for parameters
-    # @param array  $bindParameters associative array ( 'email' => 'joe@a.b' );
-    #
-    # @return int number of updated rows or 0 if failed
-    #============================================================================
-    function updateRow($sql, $bindParameters);
+//     #============================================================================
+//     # Update values into from the database
+//     #
+//     # @param string $sql the SQL string with for example ':email' for parameters
+//     # @param array  $bindParameters associative array ( 'email' => 'joe@a.b' );
+//     #
+//     # @return int number of updated rows or 0 if failed
+//     #============================================================================
+//     function updateRow($sql, $bindParameters);
 
-    #============================================================================
-    # Removes rows from the database
-    #
-    # @param string $sql the SQL string with for example ':email' for parameters
-    # @param array  $bindParameters associative array ( 'email' => 'joe@a.b' );
-    #
-    # @return int number of deleted rows or 0 if failed
-    #============================================================================
-    function deleteRows($sql, $bindParameters);
-}
+//     #============================================================================
+//     # Removes rows from the database
+//     #
+//     # @param string $sql the SQL string with for example ':email' for parameters
+//     # @param array  $bindParameters associative array ( 'email' => 'joe@a.b' );
+//     #
+//     # @return int number of deleted rows or 0 if failed
+//     #============================================================================
+//     function deleteRows($sql, $bindParameters);
+// }
 
 class CRUD
 {
-   private $conn = null;
+    /** @var PDO */
+    private $conn = null;
 
-   function __construct()
-   {
-       $this->createConnection();
-   }
+    function __construct()
+    {
+        $this->createConnection();
+    }
 
-   private function createConnection()
-   {
-      if (!$this->conn) {
-         try {
-            $conn = new PDO("mysql:host=" . dbHost . ";dbname=" . dbName . "", dbUser, dbPassword);
+    private function createConnection()
+    {
+        if (!$this->conn) {
+            try {
+                $conn = new PDO("mysql:host=" . dbHost . ";dbname=" . dbName . "", dbUser, dbPassword, []);
 
-            // set the PDO error mode to exception and default obj
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+                // set the PDO error mode to exception and default obj
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 
             } catch (PDOException $e) {
 
-               echo "Error: " . $e->getMessage();
+                echo "Error: " . $e->getMessage();
 
             }
             $this->conn = $conn;
-      }
-   }
-
-    #============================================================================   
-    public function createRow(String $sql, Array $bindParameters)
-    {
-        $stmt = $this->conn->prepare($sql);
-        
-        foreach ($bindParameters as $key => $value) {
-            $stmt->bindValue(':'.$key, $value);
         }
-        
-        $stmt->execute();
+    }
+
+    #============================================================================
+    public function createRow(String $sql, array $bindParameters)
+    {
+        $this->prepareAndExecute($sql, $bindParameters);
+
         $lastID = $this->conn->lastInsertId();
         return $lastID;
     }
 
     #============================================================================
-    function readMultiRows($sql, $bindParameters = null)
+    function readMultiRows($sql, $bindParameters = array())
     {
-        $stmt = $this->conn->prepare($sql);
-        if ($bindParameters) {
-            foreach ($bindParameters as $key => $value) {
-                $stmt->bindValue(':'.$key, $value);
-            }
-        }
-
-        $stmt->execute();
-
+        $stmt = $this->prepareAndExecute($sql, $bindParameters);
         $result = $stmt->fetchall();
-
         return $result;
     }
 
     #============================================================================
     function readOneRow($sql, $bindParameters)
     {
-        $stmt = $this->conn->prepare($sql);
-        foreach ($bindParameters as $key => $value) {
-            $stmt->bindValue(':'.$key, $value);
-        }
-
-        $stmt->execute();
-
+        $stmt = $this->prepareAndExecute($sql, $bindParameters);
         $result = $stmt->fetch();
-
         return $result;
     }
 
     #============================================================================
     function updateRow($sql, $bindParameters)
     {
-        array_push($this->sqlQueries, $sql);
-        array_push($this->bindParams, $bindParameters);
-        return 1;
+        $this->prepareAndExecute($sql, $bindParameters);
     }
 
     #============================================================================
@@ -145,6 +123,31 @@ class CRUD
         array_push($this->sqlQueries, $sql);
         array_push($this->bindParams, $bindParameters);
         return 1;
+    }
+
+    private function prepareAndExecute($sql, $bindParameters)
+    {
+        foreach ($bindParameters as $key => $value) {
+            if (is_array($value)) {
+                $partialStr = '';
+                foreach ($value as $index => $id) {
+                    if (!empty($partialStr)) {
+                        $partialStr .= ', ';
+                    }
+                    $partialStr .= ':' . $key . $index;
+                    $bindParameters[$key . $index] = $id;
+                }
+                $sql = str_replace(':' . $key, $partialStr, $sql);
+                unset($bindParameters[$key]);
+            }
+        }
+        $stmt = $this->conn->prepare($sql);
+
+        foreach ($bindParameters as $key => $value) {
+            $stmt->bindValue(':' . $key, $value);
+        }
+        $stmt->execute();
+        return $stmt;
     }
 
 }
--- incl/user_crud.php
+++ incl/user_crud.php
@@ -4,22 +4,23 @@ require_once "crud.php";
 
 class userCRUD
 {
+    /** @var CRUD*/
     private $crud = null;
 
-    public function __construct()
+    public function __construct($cruid)
     {
-        $this->crud = new CRUD();
+        $this->crud = $cruid;
     }
 
     /**
-    * writes a user to the database
-    *
-    * @param    String name to look up in the database
-    * @param    String email to look up in the database
-    * @param    String password to look up in the database
-    * @throws   Exception when failed to store user
-    */
-    function storeUser(String $name, String $email, String $password)
+     * writes a user to the database
+     *
+     * @param    String name to look up in the database
+     * @param    String email to look up in the database
+     * @param    String password to look up in the database
+     * @throws   Exception when failed to store user
+     */
+    public function storeUser(String $name, String $email, String $password)
     {
         $sql = "INSERT INTO users (name, email, password)
                 VALUES (:name, :email, :password)";
@@ -29,14 +30,14 @@ class userCRUD
     }
 
     /**
-    * reads the database and looks up a user by there email
-    *
-    * @param   String email to look up in the database
-    * @return  UserInfo UserInfo['id','name','email','password'] if found
-    * or null otherwise
-    * 
-    */
-    function findUserByEmail(String $email)
+     * reads the database and looks up a user by there email
+     *
+     * @param   String email to look up in the database
+     * @return  UserInfo UserInfo['id','name','email','password'] if found
+     * or null otherwise
+     *
+     */
+    public function findUserByEmail(String $email)
     {
         $userInfo = null;
         $sql = "SELECT * FROM users WHERE email = :email";
@@ -47,31 +48,29 @@ class userCRUD
         return $userInfo;
     }
 
-
     /**
-    * read the database and returns all the products
-    *
-    * @return array->associtive['id' => string, 'name' => string,
-    *                           'description' => string, 'price' => string,
-    *                           'image_name' => string]
-    */
-    function getProducts()
+     * read the database and returns all the products
+     *
+     * @return array->associtive['id' => string, 'name' => string,
+     *                           'description' => string, 'price' => string,
+     *                           'image_name' => string]
+     */
+    public function getProducts()
     {
         $sql = "SELECT * FROM products";
         $products = $this->crud->readMultiRows($sql);
-
         return $products;
     }
 
     /**
-    * read the database and returns all the products
-    *
-    * @param productId
-    * @return array->associtive['id' => string, 'name' => string,
-    *                           'description' => string, 'price' => string,
-    *                           'image_name' => string]
-    */
-    function getProductById($productId)
+     * read the database and returns all the products
+     *
+     * @param productId
+     * @return array->associtive['id' => string, 'name' => string,
+     *                           'description' => string, 'price' => string,
+     *                           'image_name' => string]
+     */
+    public function getProductById($productId)
     {
         $sql = "SELECT * FROM products WHERE id=:id";
         $values = array('id' => $productId);
@@ -80,48 +79,104 @@ class userCRUD
     }
 
     /**
-    *
-    * @param orderInfo array[0 => array['productId','amount','unit_price'],
-    *                        1 => array[],
-    *                        'total_price' => ]
-    * @param productId int
-    * @return void or throws
-    */
-    function storeOrder($orderInfo, $userId)
+     *
+     * @param orderInfo array[0 => array['productId','amount','unit_price'],
+     *                        1 => array[],
+     *                        'total_price' => ]
+     * @param productId int
+     * @return void or throws
+     */
+    public function storeOrder($orderInfo, $userId)
     {
-        unset($orderInfo['total_price']);
-
         $sql = "INSERT INTO orders (user_id) VALUES(:user_id)";
         $values = array('user_id' => $userId);
 
-        $order_id = $this->crud->createRow($sql,$values);
+        $order_id = $this->crud->createRow($sql, $values);
 
-        $sql =  "INSERT INTO orders_products (order_id, product_id, amount, price) 
-                            VALUES(:orders_id,:product_id,:amount,:price)";
+        $sql = "INSERT INTO orders_products (order_id, product_id, amount, price)
+                            VALUES(:order_id,:productId,:amount,:unit_price)";
 
         foreach ($orderInfo as $orderRow) {
             $orderRow['order_id'] = $order_id;
-            $this->crud->createRow($sql,$orderRow);
+            $this->crud->createRow($sql, $orderRow);
         }
+    }
+
+    /**
+     *
+     * @param productId int
+     * @return void or throws
+     */
+    public function getTop5Sold()
+    {
+        $sql = "SELECT  products.name,
+                        products.description,
+                        products.image_name,
+                        product_id,
+                        SUM(amount) AS sum_amount
+            FROM	    orders_products
+
+            JOIN        orders ON orders_products.order_id = orders.id
+            JOIN        products ON orders_products.product_id = products.id
+
+            WHERE       orders.date > ADDDATE(NOW(),INTERVAL -7 DAY)
+
+            GROUP BY    product_id
+            ORDER BY	sum_amount DESC
+            LIMIT       5;";
+
+        $products = $this->crud->readMultiRows($sql);
+
+        return $products;
+    }
+
+    public function getAvgProductRating($productIds)
+    {
+        $productIds = array("product_ids" => $productIds);
+
+        $sql = "SELECT product_id, AVG(rating) as avgRating
+                FROM ratings
+                WHERE product_id IN (:product_ids) GROUP BY product_id";
+
+        $productAvg = $this->crud->readMultiRows($sql, $productIds);
+
+        return $productAvg;
+
+    }
+
+    public function getUserRating($userId)
+    {
+        $userId = array('user_id' => $userId);
+        $sql = "SELECT product_id, rating FROM ratings WHERE user_id=:user_id";
+        $userRating = $this->crud->readMultiRows($sql,$userId);
+        return $userRating;
+    }
 
 
+    function updateOrStoreRating($productId,$rating,$userId)
+    {
+        $params = array('product_id' => $productId ,'user_id' => $userId, 'rating' => $rating);
+        // check if user already rated
+        $sql = "SELECT * FROM ratings WHERE product_id=$productId AND user_id=$userId";
+        $result = $this->crud->readMultiRows($sql,$params);
+        var_dump($result);
 
 
-        try {
-
-            $orders_id = mysqli_insert_id($conn);
-            
-
-
+        if (count($result)  > 0 ) {
+            // output data of each row
+            $sql = "UPDATE ratings SET rating=:rating WHERE product_id=:product_id AND user_id=:user_id";
+            $this->crud->updateRow($sql,$params);
+        } else {
+            $sql = "INSERT INTO ratings (product_id, user_id, rating) 
+                    VALUES(:product_id, :user_id, :rating)";
+            $this->crud->createRow($sql,$params);
+        }
     }
 
 }
 
-
-
-
-// $crud = new CRUD();
-$user_crud = new userCRUD();
+$crud = new CRUD();
+$user_crud = new userCRUD($crud);
 // $user_crud->storeUser('pim','tak@abc.com','klaasvaak');
 
 // $result = $user_crud->findUserByEmail('kip@kip.com');
@@ -130,6 +185,31 @@ $user_crud = new userCRUD();
 // $result = $user_crud->getProducts();
 // var_dump($result);
 
-
 // $result = $user_crud->getProductById(2);
 // var_dump($result);
+
+// $order = array('0' => array(
+//     'productId' => 4,
+//     'amount' => 3,
+//     'unit_price' => '10.000',
+// ));
+// $user_id = 13;
+
+// $user_crud->storeOrder($order,$user_id);
+
+// $i = $user_crud->getTop5Sold();
+// var_dump($i);
+
+// $ids = array('0' => '1',
+//     '1' => '2',
+//     '2' => '3',
+//     '3' => '4',
+//     '4' => '5',
+// );
+// $productAvg = $user_crud->getAvgProductRating($ids);
+// var_dump($productAvg);
+
+// $i = $user_crud->getUserRating('13');
+// var_dump($i);
+
+$user_crud->updateOrStoreRating(1,5,13);
